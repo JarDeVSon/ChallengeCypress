066996aed1375239f68f62de4aad7e0a0ee4ad8e
JarDeVSon <jardounifanor@gmail.com>
2024-10-07 17:56:44 -0300

    setup inicial do projeto

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', //for html reports
+  reporterOptions: {
+    reportPageTitle: 'ServRest Report',
+    embeddedScreenshots: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    defaultBrowser: 'chrome',
+    viewportWidth: 1280,
+    viewportHeight: 768,
+    waitForAnimations: true,
+    defaultCommandTimeout: 30000,
+    pageLoadTimeout: 30000,
+    requestTimeout: 6000,
+    responseTimeout: 6000,
+    video: true,
+    env: {
+      requestMode: true,
+    },
+
+  },
+})
